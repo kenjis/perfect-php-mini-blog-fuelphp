52f139a3d1db92c6b69c7f02d6da6266d782e223
kenjis <kenji.uui@gmail.com>
2012-07-23 20:01:21 +0900

    ビューの修正
    リンクの URL の修正。コーディングスタイルの修正

--- fuel/app/views/account/signup.php
+++ fuel/app/views/account/signup.php
@@ -1,27 +1,21 @@
 <h2>アカウント登録</h2>
 
-<form action="<?php echo Uri::base(); ?>/account/register" method="post">
-<input type="hidden" name="<?php echo \Config::get('security.csrf_token_key');?>" value="<?php echo \Security::fetch_token();?>" />
-
-
+<form action="<?php echo Uri::base(); ?>account/register" method="post">
+<input type="hidden" name="<?php echo Config::get('security.csrf_token_key');?>" value="<?php echo Security::fetch_token();?>" />
 
     <?php if (isset($errors) && count($errors) > 0): ?>
     <?php
-        $data = array(
-            'errors' => $errors,
-        );
-        echo View::forge('errors', $data);
+    echo View::forge('errors', array('errors' => $errors));
     ?>
     <?php endif; ?>
 
     <?php
-        $data = array(
-            'user_name' => $user_name,
-            'password' => $password,
-        );
-        echo View::forge('account/inputs', $data);
+    $data = array(
+        'user_name' => $user_name,
+        'password'  => $password,
+    );
+    echo View::forge('account/inputs', $data);
     ?>
-
 
     <p>
         <input type="submit" value="登録" />
