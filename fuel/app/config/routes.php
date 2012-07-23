f2b2c1cc34f9d4d6ccc009890cab3afc8f684a1e
kenjis <kenji.uui@gmail.com>
2012-07-23 18:30:38 +0900

    ルーティング設定

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,7 +1,11 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
+	'_root_'  => 'status/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
 	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+	//'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+
+	'user/:user_name/status/:id' => 'status/show',
+	'user/:user_name'            => 'status/user',
+	'follow'                     => 'account/follow',
 );
